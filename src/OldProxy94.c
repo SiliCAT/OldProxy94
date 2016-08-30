ceea7aaedac7f883af71c8b67f254625ff62e535
silicat <silicat@sina.com>
2016-08-30 15:53:38 -0400

    Test

---
+++ src/OldProxy94.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : OldProxy94.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("OldProxy"); /* prints OldProxy */
+	return EXIT_SUCCESS;
+}
